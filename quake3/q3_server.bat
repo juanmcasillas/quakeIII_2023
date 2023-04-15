604780c2bdc82c4ff62274efae9bc2c78d8e961b
Juan M. Casillas <juanm.casillas@gmail.com>
2023-04-15 20:56:46 +0200

    fixed the mem to 1024 MB

--- quake3/q3_server.bat
+++ quake3/q3_server.bat
@@ -1,2 +1,2 @@
 REM https://ioquake3.org/help/sys-admin-guide/
-.\quake3e.ded.x64 +set dedicated 1 +set net_port 27969 +com_hunkMegs 128 +exec server.cfg
+.\quake3e.ded.x64 +set dedicated 1 +set net_port 27969 +com_hunkMegs 1024 +exec server.cfg
